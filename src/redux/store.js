133701dfd69ca25a00f2438f5d67f216c33beaf1
Łukasz Dawidowicz <lddev@outlook.com>
2020-04-05 13:20:37 +0200

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+// initial state
+const initialState = {};
+
+// define reducers
+const reducers = {};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+// merge all reducers
+const storeReducer = combineReducers(reducers);
+
+// create store
+const store = createStore(
+  storeReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
